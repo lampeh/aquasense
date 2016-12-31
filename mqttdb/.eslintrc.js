f36592b139bbab19e537216d8751c43b1c78d181
Hauke Lampe <lampe@hauke-lampe.de>
2016-12-31 16:46:25 +0100

    sorted .eslintrc.js

--- mqttdb/.eslintrc.js
+++ mqttdb/.eslintrc.js
@@ -5,47 +5,35 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": {
-        "indent": [
-            "warn",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
+        "arrow-body-style": "warn",
+        "arrow-parens": "warn",
+        "arrow-spacing": "warn",
+        "complexity": "warn",
+        "curly": "warn",
+        "dot-location": [ "warn", "property" ],
+        "eqeqeq": "warn",
+        "indent": [ "warn", "tab" ],
+        "linebreak-style": [ "error", "unix" ],
+        "no-confusing-arrow": "warn",
         "no-console": "off",
+        "no-duplicate-imports": "warn",
+        "no-duplicate-imports": "warn",
+        "no-multi-spaces": "warn",
+        "no-prototype-builtins": "warn",
+        "no-template-curly-in-string": "warn",
+        "no-unsafe-negation": "warn",
+        "no-useless-computed-key": "warn",
+        "no-useless-constructor": "warn",
+        "no-var": "warn",
         "prefer-arrow-callback": "warn",
         "prefer-const": "warn",
         "prefer-numeric-literals": "warn",
         "prefer-rest-params": "warn",
         "prefer-spread": "warn",
         "prefer-template": "warn",
-        "arrow-body-style": "warn",
-        "arrow-parens": "warn",
-        "arrow-spacing": "warn",
-        "no-confusing-arrow": "warn",
-        "no-duplicate-imports": "warn",
-        "no-duplicate-imports": "warn",
-        "no-useless-computed-key": "warn",
-        "no-useless-constructor": "warn",
-        "no-var": "warn",
-        "yoda": "warn",
-        "complexity": "warn",
-        "curly": "warn",
-        "dot-location": [ "warn", "property" ],
-        "eqeqeq": "warn",
-        "no-multi-spaces": "warn",
-        "no-prototype-builtins": "warn",
-        "no-template-curly-in-string": "warn",
-        "no-unsafe-negation": "warn",
-        "strict": "error"
+        "quotes": [ "error", "double" ],
+        "semi": [ "error", "always" ],
+        "strict": "error",
+        "yoda": "warn"
     }
 };
